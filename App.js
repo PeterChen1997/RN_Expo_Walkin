710c2fb3a8fa6ef24b8a78d332976222839ff987
Peter Chen <13093023233@163.com>
2022-11-08 21:32:20 +0800

    feat: init

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [currentGoal, setCurrentGoal] = useState("123");
+    const [goals, setGoals] = useState([]);
+
+    const handleChange = (value) => {
+        setCurrentGoal(value);
+    };
+
+    const handleGoalAdd = () => {
+        console.log(currentGoal);
+        setGoals([...goals, currentGoal]);
+    };
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={styles.container}>
+                    <TextInput
+                        placeholder="Please input your goal"
+                        onChangeText={handleChange}
+                        style={styles.input}
+                    />
+                    <Button title={"add"} onPress={handleGoalAdd}></Button>
+                </View>
+
+                <View style={styles.goalsContainer}>
+                    {goals.map((goal) => (
+                        <View style={styles.goal}>
+                            <Text>{goal}</Text>
+                        </View>
+                    ))}
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    input: {
+        borderColor: "gray",
+        borderWidth: 1,
+        padding: 10,
+        width: "70%",
+        marginRight: 10,
+    },
+    container: {
+        flexDirection: "row",
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 20,
+    },
+    goal: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: "#ccc",
+        borderColor: "gray",
+        borderWidth: 1,
+        width: "90%",
+    },
+    goalsContainer: {
+        alignItems: "center",
+        width: "100%",
+    },
 });
